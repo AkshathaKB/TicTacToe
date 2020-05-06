014cfd52926597e459e3fcc7359d508e9962b7da
AkshathaKB <akshathabanakar004@gmail.com>
2020-05-06 19:10:30 -0400

    [Akshatha]added. Assigning the symbol to the players

---
+++ UseCase2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+function reset(){
+	echo "Welcome to TicTacToe game"
+	Arr=(. . . . . . . . .)
+	player=1
+	gamestatus=1
+	echo "===================="
+	echo "Game started"
+}
+
+function player(){
+
+	player = "0";
+	computer = "X";
+}
+
+
+
