4b459e99e68a380c59dc2f0d49a2425ab592c33e
AkshathaKB <akshathabanakar004@gmail.com>
2020-05-06 19:58:56 -0400

    [Akshatha]added. Displaying the Board

---
+++ UseCase4.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+function reset(){
+	echo "Welcome to TicTacToe game"
+	Arr=(. . . . . . . . .)
+	player=1
+	gamestatus=1
+	echo "===================="
+	echo "Game started"
+}
+
+function checkSymbol(){
+	player = "0";
+	computer="X";
+}
+
+function set(){
+    idx = $(( $1 * 3 + $2 ))
+	if [ ${Arr[$idx]} == "."];
+	then
+	Arr[$idx]=$3
+	player=$((player%2+1))
+	else
+	echo "You can't place there!"
+	fi
+}
+
+function displayingBoard(){
+	echo "r\c 0 1 2"
+	echo "0 ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+	echo "0 ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+	echo "0 ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+}
+
+
+
+
+
